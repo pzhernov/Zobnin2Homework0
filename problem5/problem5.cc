30527fa1ce1aff79288a949e35b6cf746a942a80
Pavel Zhernov <linux.forever.root@gmail.com>
2011-02-25 12:40:01 +0000

    some changes made in interface

---
+++ problem5/problem5.cc
@@ -0,0 +1,1 @@
+#include "problem5.h"
--- problem5/problem5.h
+++ problem5/problem5.h
@@ -8,18 +8,67 @@ class ContainerPointerInterface {
 
   template <typename ContainerType>
   virtual const ContainerType* const GetPointer() const = 0;
+  virtual size_t GetSize() const = 0;
 };
 
 //! -----ContainerPointer declaration-----
 template <typename ContainerType>
-class ContainerPointer : public ContainerPointerInterface {
+class ContainerPointer<ContainerType> : public ContainerPointerInterface {
  public:
   ContainerPointer(const ContainerType& container);
   virtual ~ContainerPointer();
   virtual const ContainerType* const GetPointer() const;
+  virtual size_t GetSize() const;
 
  private:
   ContainerType* pointer_;
+  size_t size_;
+};
+
+template <typename ContainerType>
+class ContainerPointer<ContainerType*> : public ContainerPointerInterface {
+ public:
+  ContainerPointer(const ContainerType* container, size_t size);
+  virtual ~ContainerPointer();
+  virtual const ContainerType* const GetPointer() const;
+  virtual size_t GetSize() const;
+
+ private:
+  ContainerType* pointer_;
+  size_t size_;
+};
+
+//! -----StandardContainerIteratorInterface declaration-----
+class StandardContainerIteratorInterface {
+ public:
+  StandardContainerIteratorInterface();
+  virtual ~StandardContainerIteratorInterface();
+
+  template <typename ContainerIteratorType>
+  virtual ContainerIteratorType GetIterator() const = 0;
+};
+
+//! -----StandardContainerIterator declaration-----
+template <typename ContainerType>
+class StandardContainerIterator<ContainerType> : public StandardContainerIteratorInterface {
+ public:
+  StandardContainerIterator(const typename ContainerType::iterator& container_iterator);
+  virtual ~StandardContainerIterator();
+  virtual typename ContainerType::iterator& GetIterator() const;
+
+ private:
+  typename ContainerType::iterator container_iterator_;
+};
+
+template <typename ContainerType>
+class StandardContainerIterator<ContainerType*> : public StandardContainerIteratorInterface {
+ public:
+  StandardContainerIterator(const ContainerType* container_iterator);
+  virtual ~StandardContainerIterator();
+  virtual ContainerType*& const GetIterator() const;
+
+ private:
+  ContainerType* container_iterator_;
 };
 
 //! -----IntegratedContainer declaration-----
@@ -37,8 +86,6 @@ class IntegratedContainer {
 
     template <typename ContainerType>
     ContainerIterator(const ContainerType& container);
-
-    ContainerIterator(const ContainerIterator& container_iterator);
 
     ContainerIterator& operator++();
     ContainerIterator& operator--();
@@ -65,7 +112,7 @@ class IntegratedContainer {
     template <typename ElementType>
     ElementIterator(const ElementType& element);
 
-    ElementIterator(const ElementIterator& element_iterator);
+    //ElementIterator(const ElementIterator& element_iterator);
 
     ElementIterator& operator++();
     ElementIterator& operator--();
@@ -80,7 +127,7 @@ class IntegratedContainer {
 
    private:
     typename std::list<ContainerPointerInterface*>::iterator container_iterator_;
-    typename //TODO
+    StandardContainerIteratorInterface* element_iterator_;
   };
 
  private:
@@ -99,11 +146,11 @@ class IntegratedContainer {
 
 //! -----ContainerPointerInterface implementation-----
 inline
-ContainerPointerInterface<ContainerType>::ContainerPointerInterface() {
+ContainerPointerInterface::ContainerPointerInterface() {
 }
 
 inline
-ContainerPointerInterface<ContainerType>::~ContainerPointerInterface() {
+ContainerPointerInterface::~ContainerPointerInterface() {
 }
 
 //! -----ContainerPointer implementation-----
@@ -112,7 +159,8 @@ inline
 ContainerPointer<ContainerType>::ContainerPointer(
     const ContainerType& container)
   : ContainerPointerInterface()
-  , pointer_(&container) {
+  , pointer_(&container)
+  , size_(container->size()) {
 }
 
 template <typename ContainerType>
@@ -124,6 +172,38 @@ template <typename ContainerType>
 inline
 const ContainerType* const ContainerPointer<ContainerType>::GetPointer() const {
   return pointer_;
+}
+
+template <typename ContainerType>
+inline
+size_t ContainerPointer<ContainerType>::GetSize() const {
+  return size_;
+}
+
+template <typename ContainerType*>
+inline
+ContainerPointer<ContainerType*>::ContainerPointer(
+    const ContainerType& container, size_t size)
+  : ContainerPointerInterface()
+  , pointer_(&container)
+  , size_(size) {
+}
+
+template <typename ContainerType*>
+inline
+ContainerPointer<ContainerType*>::~ContainerPointer() {
+}
+
+template <typename ContainerType>
+inline
+const ContainerType* const ContainerPointer<ContainerType*>::GetPointer() const {
+  return pointer_;
+}
+
+template <typename ContainerType>
+inline
+size_t ContainerPointer<ContainerType*>::GetSize() const {
+  return size_;
 }
 
 //! -----IntegratedContainer implementation-----
